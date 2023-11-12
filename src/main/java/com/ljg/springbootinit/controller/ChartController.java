b0c6603eed52b4b51ca87ba79bb7bb3e365bea1e
Li_Jin_Gang <2012427202@qq.com>
2023-11-12 19:38:28 +0800

    图表管理功能，Redisson 限流实现

--- src/main/java/com/ljg/springbootinit/controller/ChartController.java
+++ src/main/java/com/ljg/springbootinit/controller/ChartController.java
@@ -1,5 +1,6 @@
 package com.ljg.springbootinit.controller;
 
+import cn.hutool.core.io.FileUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.google.gson.Gson;
@@ -13,6 +14,7 @@ import com.ljg.springbootinit.constant.UserConstant;
 import com.ljg.springbootinit.exception.BusinessException;
 import com.ljg.springbootinit.exception.ThrowUtils;
 import com.ljg.springbootinit.manager.AiManager;
+import com.ljg.springbootinit.manager.RedisLimiterManager;
 import com.ljg.springbootinit.model.dto.chart.*;
 import com.ljg.springbootinit.model.entity.Chart;
 import com.ljg.springbootinit.model.entity.User;
@@ -30,6 +32,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 图表接口
@@ -46,7 +50,8 @@ public class ChartController {
     private UserService userService;
     @Resource
     private AiManager aiManager;
-
+    @Resource
+    private RedisLimiterManager redisLimiterManager;
     // region 增删改查
 
     /**
@@ -240,6 +245,7 @@ public class ChartController {
      * 智能分析
      */
     @PostMapping("/gen")
+// 把返回值改成BiResponse
     public BaseResponse<BiResponse> genChartByAi(@RequestPart("file") MultipartFile multipartFile,
                                                  GenChartByAiRequest genChartByAiRequest, HttpServletRequest request) {
         String name = genChartByAiRequest.getName();
@@ -251,21 +257,55 @@ public class ChartController {
         ThrowUtils.throwIf(StringUtils.isBlank(goal), ErrorCode.PARAMS_ERROR, "目标为空");
         // 如果名称不为空，并且名称长度大于100，就抛出异常，并给出提示
         ThrowUtils.throwIf(StringUtils.isNotBlank(name) && name.length() > 100, ErrorCode.PARAMS_ERROR, "名称过长");
+
+        /*
+          校验文件
+
+          首先,拿到用户请求的文件;
+          取到原始文件大小
+         */
+        long size = multipartFile.getSize();
+        // 取到原始文件名
+        String originalFilename = multipartFile.getOriginalFilename();
+
+        /*
+          校验文件大小
+
+          定义一个常量表示1MB;
+          一兆(1MB) = 1024*1024字节(Byte) = 2的20次方字节
+         */
+        final long ONE_MB = 1024 * 1024L;
+        // 如果文件大小,大于一兆,就抛出异常,并提示文件超过1M
+        ThrowUtils.throwIf(size > ONE_MB, ErrorCode.PARAMS_ERROR, "文件超过 1M");
+
+        /*
+          校验文件后缀(一般文件是aaa.png,我们要取到.<点>后面的内容)
+          利用FileUtil工具类中的getSuffix方法获取文件后缀名(例如:aaa.png,suffix应该保存为png)
+         */
+        String suffix = FileUtil.getSuffix(originalFilename);
+        // 定义合法的后缀列表
+        final List<String> validFileSuffixList = Arrays.asList("png", "jpg", "svg", "webp", "jpeg");
+        // 如果suffix的后缀不在List的范围内,抛出异常,并提示'文件后缀非法'
+        ThrowUtils.throwIf(!validFileSuffixList.contains(suffix), ErrorCode.PARAMS_ERROR, "文件后缀非法");
+
         // 通过response对象拿到用户id(必须登录才能使用)
         User loginUser = userService.getLoginUser(request);
 
+        // 限流判断，每个用户一个限流器
+        redisLimiterManager.doRateLimit("genChartByAi_" + loginUser.getId());
+
         // 指定一个模型id(把id写死，也可以定义成一个常量)
-        long biModelId = 1722951338394284033L;
-        /*
-         * 用户的输入(参考)
-            分析需求：
-            分析网站用户的增长情况
-            原始数据：
-            日期,用户数
-            1号,10
-            2号,20
-            3号,30
-        **/
+        long biModelId = 1659171950288818178L;
+    /*
+    * 用户的输入(参考)
+      分析需求：
+      分析网站用户的增长情况
+      原始数据：
+      日期,用户数
+      1号,10
+      2号,20
+      3号,30
+    * */
 
         // 构造用户输入
         StringBuilder userInput = new StringBuilder();
@@ -292,24 +332,40 @@ public class ChartController {
         if (splits.length < 3) {
             throw new BusinessException(ErrorCode.SYSTEM_ERROR, "AI 生成错误");
         }
-
+        // 如果是正确就提取图表代码(删除多余的空格和换行)
         String genChart = splits[1].trim();
+        // 如果是正确的就提取结论(删除多余的空格和换行)
         String genResult = splits[2].trim();
+
         // 插入到数据库
         Chart chart = new Chart();
+        // 图表名称
         chart.setName(name);
+        // 分析目标
         chart.setGoal(goal);
+        // 图表数据
         chart.setChartData(csvData);
+        // 图表类型
         chart.setChartType(chartType);
+        // 图表代码
         chart.setGenChart(genChart);
+        // 结论
         chart.setGenResult(genResult);
+        // 谁登录的，就是谁创建的
         chart.setUserId(loginUser.getId());
+        // 如果为真，保存图表到数据库中
         boolean saveResult = chartService.save(chart);
+        // 如果为假，抛出异常，并提示"图表保存失败"
         ThrowUtils.throwIf(!saveResult, ErrorCode.SYSTEM_ERROR, "图表保存失败");
+
+        // 最后封装到BiResponse里
         BiResponse biResponse = new BiResponse();
+        // 设置一下拿到的结果
         biResponse.setGenChart(genChart);
         biResponse.setGenResult(genResult);
+        // 把新生成的图表id拿到
         biResponse.setChartId(chart.getId());
+        // 最后，返回biResponse
         return ResultUtils.success(biResponse);
     }
 }
--- src/main/java/com/ljg/springbootinit/mapper/ChartMapper.java
+++ src/main/java/com/ljg/springbootinit/mapper/ChartMapper.java
@@ -1,16 +1,26 @@
 package com.ljg.springbootinit.mapper;
 
-import com.ljg.springbootinit.model.entity.Chart;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ljg.springbootinit.model.entity.Chart;
+
+import java.util.List;
+import java.util.Map;
 
 /**
-* @author 86137
-* @description 针对表【chart(图表信息表)】的数据库操作Mapper
-* @createDate 2023-11-05 20:45:27
-* @Entity com.ljg.springbootinit.model.entity.Chart
-*/
+ * @author 86137
+ * @description 针对表【chart(图表信息表)】的数据库操作Mapper
+ * @createDate 2023-11-05 20:45:27
+ * @Entity com.ljg.springbootinit.model.entity.Chart
+ */
 public interface ChartMapper extends BaseMapper<Chart> {
-
+    /*
+     * 方法的返回类型是 List<Map<String, Object>>,
+     * 表示返回的是一个由多个 map 组成的集合,每个map代表了一行查询结果，
+     * 并将其封装成了一组键值对形式的对象。其中,String类型代表了键的类型为字符串，
+     * Object 类型代表了值的类型为任意对象,使得这个方法可以适应不同类型的数据查询。
+     *
+     */
+    List<Map<String, Object>> queryChartData(String querySql);
 }
 
 
